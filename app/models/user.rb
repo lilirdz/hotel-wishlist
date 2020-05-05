314d4ef2c760f0e3741c243f9385d15719275c1b
teeman60 <taiye41@gmail.com>
2020-05-05 11:46:59 -0500

    empty array for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
     has_many :lists
-    has_many :hotels, through: :lists
+    has_many :hotels, through :lists
+
+    wishlist = [] #would this make sense?
 end
--- db/migrate/002_create_hotels.rb
+++ db/migrate/002_create_hotels.rb
@@ -2,7 +2,6 @@ class CreateHotels < ActiveRecord::Migration[5.2]
     def change 
         create_table :hotels do |t|
             t.string :hotel_name
-            t.string :address
             t.string :city
             t.integer :avg_rating
             t.integer :price
