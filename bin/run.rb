1806eff83b6c492a16ad269d31727ae4c049e821
lilirdz <lilimrod@gmail.com>
2020-05-05 17:55:26 -0500

    added seeds and welcome method to run file

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,11 @@
 require_relative '../config/environment'
+require "tty-prompt"
 
+@prompt = TTY::Prompt.new
 
-
-puts "HELLO WORLD"
+def welcome
+    puts "Hello! Welcome to your Hotel Wishlist!"
+    user_input = @prompt.ask('What is your name?',required: true)
+end
+@test = welcome
+puts test
