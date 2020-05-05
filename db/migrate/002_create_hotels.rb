405656cc4a52bc84ecc975236546e6b1b179e424
lilirdz <lilimrod@gmail.com>
2020-05-04 21:36:31 -0500

    added models and migrations

---
+++ db/migrate/002_create_hotels.rb
@@ -0,0 +1,14 @@
+class CreateHotels < ActiveRecord::Migration[5.2]
+    def change 
+        create_table :hotels do |t|
+            t.string :hotel_name
+            t.string :address
+            t.string :city
+            t.integer :avg_rating
+            t.integer :price
+            t.boolean :family_rooms
+            t.boolean :smoking_rooms
+        end
+    end
+
+end
