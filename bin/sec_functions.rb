799d40b2303b25b5b965d18ddceb753594d387af
teeman60 <taiye41@gmail.com>
2020-05-08 08:03:46 -0500

    updated  rating recommendation in functions.rb

--- bin/sec_functions.rb
+++ bin/sec_functions.rb
@@ -2,11 +2,10 @@ require_relative '../config/environment'
 
 
 require "tty-prompt"
-require_all 'app'
 
 @prompt = TTY::Prompt.new
 
-
+# $prompt.ask('What is your name?')
 
 def choose_city
     cities = ["Houston", "LA", "Las Vegas", "Miami","Chicago"]
@@ -16,7 +15,6 @@ def choose_city
     filtered_hotels.each do |x|
        puts "Hotel: #{x.hotel_name}, Address: #{x.address}, City: #{x.city}, Average Rating: #{x.avg_rating}, Price: #{x.price}"
     end
-    more_filters(user)   # I added this to call the next function
 end
 
 def more_filters?(user)
@@ -27,7 +25,7 @@ def more_filters?(user)
         selection = @prompt.ask("Which hotel would you like to add to your list?")
         chosen_hotel = Hotel.find_by(hotel_name: selection)
         user.add_hotel(chosen_hotel)
-        #ty's code goes here (this is not necessary since my recommendations code is targeted for a particular city)
+        #ty's code goes here
         
     end
 end
@@ -69,10 +67,6 @@ def choose_preferences(user)
     end
 end
 
-
-
-#     TAIYE  start
-
 def recommend(select_rating)                           #can we pass a method into a string using {} ?
     
     if  @select_rating == 4
@@ -80,11 +74,11 @@ def recommend(select_rating)                           #can we pass a method int
         hotelfivestar(@response, @select_city)
     elsif @select_rating == 3
         @response = $prompt.yes?("Great! Would you mind checking out another with a higher max. occupancy?")
-        hotelfourstar(@response, @select_city)
+        hotelfourstar(@response)
     else
         puts "You've made an option for the highest-rated hotel in your city"
         $prompt.yes?('Have you visited this hotel previously?')
-        # yesorno(@ans)
+        yesorno(@ans)
         puts "Your entry has been saved 😍"
         
     end
@@ -101,7 +95,7 @@ def hotelfivestar(response, select_city)
         $prompt.select("Choose an option", [option], active_color: :on_bright_red)
         puts "This is the absolute BEST hotel in town"
         @ans = $prompt.yes?('Have you visited this hotel previously?')
-        # yesorno(@ans)
+        yesorno(@ans)
         puts "Your entry has been saved 😍"
         
     end 
@@ -109,13 +103,13 @@ def hotelfivestar(response, select_city)
         
 end
 
-# def yesorno(ans)
-#     if @ans == true
-#         #update visited? column to "Yes" for this hotel
-#     elsif @ans == false
-#         #update visited? column to "No" for this hotel
-#     end
-# end
+def yesorno(ans)
+    if @ans == true
+        #update visited? column to "Yes" for this hotel
+    elsif @ans == false
+        #update visited? column to "No" for this hotel
+    end
+end
 
 
 def hotelfourstar(response, select_city)
@@ -125,38 +119,70 @@ def hotelfourstar(response, select_city)
         $prompt.select("Choose an option", [option], active_color: :on_bright_red)
         puts "Great choice! You'll absolutely love it there"
         @ans = $prompt.yes?('Have you visited this hotel previously?')
-        # yesorno(@ans)
+        yesorno(@ans)
         puts "Your entry has been saved 😍"
     end
 end
 
 
 
-#       TAIYE end
-
-
-def delete_options(user)
-    if user.hotels.empty?
-        puts "You have no hotels saved."
-    else
-        choices = ["Delete hotels by city","Delete hotel by name", "Delete all hotels"]
-        user_input = @prompt.select("Choose an option",choices)
-        if user_input == choices[0]
-            cities = ["Houston", "LA", "Las Vegas", "Miami","Chicago"]
-            choose_city = @prompt.select("Which city?",cities)
-            user.delete_hotels_by_location(choose_city)
-            puts "All hotels in #{choose_city} have been removed from your list."
-        elsif user_input == choices[1]
-            user.display_list
-            choose_hotel = @prompt.ask("Which hotel do you want to delete?")
-            user.delete_hotel(choose_hotel)
-            puts "#{choose_hotel} has been removed from your list."
-        elsif user_input == choices[2]
-            user.delete_all_hotels
-            puts "All hotels have been removed from your list."
-        end
+def houston 
+    arr = Hotel.where("city==?","Houston")    #this prints out the *names* of hotels in the chosen city
+    arr.map do |x|
+        x.hotel_name
     end
 end
 
+def la 
+    arr = Hotel.where("city==?","LA")    #this prints out the *names* of hotels in the chosen city
+    arr.map do |x|
+        x.hotel_name
+    end
+end
+
+def lasvegas 
+    arr = Hotel.where("city==?","Las Vegas")    #this prints out the *names* of hotels in the chosen city
+    arr.map do |x|
+        x.hotel_name
+    end
+end
+
+def miami 
+    arr = Hotel.where("city==?","Miami")    #this prints out the *names* of hotels in the chosen city
+    arr.map do |x|
+        x.hotel_name
+    end
+end
+
+def chicago 
+    arr = Hotel.where("city==?","Chicago")    #this prints out the *names* of hotels in the chosen city
+    arr.map do |x|
+        x.hotel_name
+    end
+end
 
 choose_city
+
+
+# def delete_options(user)
+#     if user.hotels.empty?
+#         puts "You have no hotels saved."
+#     else
+#         choices = ["Delete hotels by city","Delete hotel by name", "Delete all hotels"]
+#         user_input = @prompt.select("Choose an option",choices)
+#         if user_input == choices[0]
+#             cities = ["Houston", "LA", "Las Vegas", "Miami","Chicago"]
+#             choose_city = @prompt.select("Which city?",cities)
+#             user.delete_hotels_by_location(choose_city)
+#             puts "All hotels in #{choose_city} have been removed from your list."
+#         elsif user_input == choices[1]
+#             user.display_list
+#             choose_hotel = @prompt.ask("Which hotel do you want to delete?")
+#             user.delete_hotel(choose_hotel)
+#             puts "#{choose_hotel} has been removed from your list."
+#         elsif user_input == choices[2]
+#             user.delete_all_hotels
+#             puts "All hotels have been removed from your list."
+#         end
+#     end
+# end
